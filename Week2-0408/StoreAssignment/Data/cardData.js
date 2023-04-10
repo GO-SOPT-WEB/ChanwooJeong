240b02fb9df39868b73b49cda5a545d761c0f795
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-10 17:46:25 +0900

    [chore] : 포켓몬 카드 Data 추가

---
+++ Week2-0408/StoreAssignment/Data/cardData.js
@@ -0,0 +1,45 @@
+const POKEMON_LIST = [
+  {
+    name: "이상해씨",
+    tag: ["진화1", "귀여운", "잎사귀", "식물성"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/000101.png",
+    JJim: false,
+    evolution: 1,
+  },
+  {
+    name: "피캬츄",
+    tag: ["진화1", "번개", "주인공", "뿅"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/002501.png",
+    JJim: false,
+    evolution: 1,
+  },
+
+  {
+    name: "날쌩마",
+    tag: ["진화2", "유니콘", "얼룩말"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/007802.png",
+    JJim: false,
+    evolution: 2,
+  },
+  {
+    name: "어니부기",
+    tag: ["진화2", "꼬북이", "물"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/000801.png",
+    JJim: false,
+    evolution: 2,
+  },
+  {
+    name: "리자몽",
+    tag: ["진화3", "불속성", "난폭한"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/000604.png",
+    JJim: false,
+    evolution: 3,
+  },
+  {
+    name: "푸크린",
+    tag: ["진화3", "노래방", "잠자는"],
+    img: "https://data1.pokemonkorea.co.kr/newdata/pokedex/mid/004001.png",
+    JJim: false,
+    evolution: 3,
+  },
+];
