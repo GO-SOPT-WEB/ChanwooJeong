abfc5c07c4c2498078009684391d23f01fdbb36f
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-10 21:40:55 +0900

    [feat] : 필터에 맞는 포켓몬카드 거른 후  template태그를 이용해 카드생성

--- Week2-0408/StoreAssignment/myStore.js
+++ Week2-0408/StoreAssignment/myStore.js
@@ -3,41 +3,69 @@ const categoryArray = Array.from(navCategorys);
 
 const activeNav = ["all"];
 
-const createNavTagItem = (name) => {
-  let navTagName = "";
-  switch (name) {
-    case "all":
-      navTagName = "전체";
-      break;
-    case "evolution1":
-      navTagName = "진화1";
-      break;
-    case "evolution2":
-      navTagName = "진화2";
-      break;
-    case "evolution3":
-      navTagName = "진화3";
-      break;
-    default:
-      alert("맞는 nav가 없습니다.");
+//처음 로딩시 전체 카드 노출
+window.onload = function () {
+  addCategorySection(activeNav);
+  const filteredCard = filterCard(activeNav);
+  addCardItem(filteredCard);
+};
+
+// 필터에 맞는 카드 배열에 담아서 return 함수
+const filterCard = (activeNav) => {
+  let filterdCard = [];
+  if (activeNav.indexOf("all") !== -1) {
+    return POKEMON_LIST;
+  } else {
+    activeNav.map((activefilter) => {
+      const evolutionN = activefilter.charAt(activefilter.length - 1);
+      return POKEMON_LIST.filter((card) => {
+        if (card.evolution === Number(evolutionN))
+          return filterdCard.push(card);
+      });
+    });
   }
+  return filterdCard;
+};
 
-  let li = document.createElement("li");
-  li.className = name;
-  li.innerHTML = `${navTagName} &#10008; `;
-  li.addEventListener("click", () => {
-    const index = activeNav.indexOf(name);
-    activeNav.splice(index, 1);
-    const checkbox = document.querySelectorAll(`#${name}`)[0];
-    console.log(checkbox);
-    checkbox.checked = !checkbox.checked;
-    addCategorySection(activeNav);
-  });
-  return li;
-}
-
+// navFilterSection 에 필터종류 노출 및 삭제
 const addCategorySection = (activeNavArray) => {
   const tagUl = document.querySelectorAll(".tagUl")[0];
+
+  const createNavTagItem = (name) => {
+    let navTagName = "";
+    switch (name) {
+      case "all":
+        navTagName = "전체";
+        break;
+      case "evolution1":
+        navTagName = "진화1";
+        break;
+      case "evolution2":
+        navTagName = "진화2";
+        break;
+      case "evolution3":
+        navTagName = "진화3";
+        break;
+      default:
+        alert("맞는 nav가 없습니다.");
+    }
+
+    let li = document.createElement("li");
+    li.className = name;
+    li.innerHTML = `${navTagName} &#10008; `;
+
+    li.addEventListener("click", () => {
+      const index = activeNav.indexOf(name);
+      activeNav.splice(index, 1);
+      const checkbox = document.querySelectorAll(`#${name}`)[0];
+      checkbox.checked = !checkbox.checked;
+      addCategorySection(activeNav);
+      const filteredCard = filterCard(activeNav);
+      addCardItem(filteredCard);
+    });
+
+    return li;
+  };
 
   while (tagUl.firstChild) {
     tagUl.removeChild(tagUl.firstChild);
@@ -47,7 +75,37 @@ const addCategorySection = (activeNavArray) => {
   });
 };
 
-addCategorySection(activeNav);
+//필터에 따른 카드 생성 함수
+const addCardItem = (filteredCard) => {
+  const cardTemplate = document.querySelector("#temp-card");
+  const div = document.createElement("div");
+  div.innerHTML = cardTemplate.innerHTML;
+
+  const cardSection = document.querySelector("#cardSection");
+
+  while (cardSection.firstChild) {
+    cardSection.removeChild(cardSection.firstChild);
+  }
+
+  filteredCard.map((card) => {
+    const element = div.cloneNode(true);
+    const tagContainer = element.querySelectorAll(".tagContainer")[0];
+
+    card.tag.map((tag) => {
+      let li = document.createElement("li");
+      li.innerHTML = tag;
+      li.className = "tag";
+      tagContainer.appendChild(li);
+    });
+
+    let html = element.innerHTML;
+    html = html.replace("{pokemonName}", card.name);
+    html = html.replace("{img}", card.img);
+
+    element.innerHTML = html;
+    cardSection.appendChild(element);
+  });
+};
 
 // navCategory 클릭시 category 활성화
 categoryArray.map((navCatgory) => {
@@ -63,5 +121,7 @@ categoryArray.map((navCatgory) => {
     }
 
     addCategorySection(activeNav);
+    const filteredCard = filterCard(activeNav);
+    addCardItem(filteredCard);
   });
 });
