e2764d0cf6b4039b44fab35b0ae9498af9c0b669
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-13 01:52:00 +0900

    [feat] : category drag&drop 기능 구현

--- Week2-0408/WebTodoAssignment/jsToReact/src/pages/mycategory.js
+++ Week2-0408/WebTodoAssignment/jsToReact/src/pages/mycategory.js
@@ -1,13 +1,26 @@
+import "../CSS/mycategory.css";
+import "../JSfile/mycategory";
 function MyCategory($container) {
-    this.$container = $container;
-  
-    this.render = () => {
-      this.$container.innerHTML = ` 
-         MyCategory
+  this.$container = $container;
+
+  this.render = () => {
+    this.$container.innerHTML = ` 
+      <div id="changeCategoryContainer">
+        <header>
+            <h1>❤️Change Todo Category Index❤️</h1>
+
+            
+        </header>
+
+        <div class="dragDropcontainer">
+        
+        </div>
+      
+      </div>
+
     `;
-    };
-    this.render();
-  }
-  
-  export default MyCategory;
-  
+  };
+  this.render();
+}
+
+export default MyCategory;
