9db2afbab7a2c6305dad9f8b05891c376b44b495
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-14 21:36:13 +0900

    [Edit] : 배포를 위한 href 수정

--- Week2-0408/StoreAssignment/addpokemon.js
+++ Week2-0408/StoreAssignment/addpokemon.js
@@ -51,5 +51,5 @@ registButton.addEventListener("click", () => {
     `mypokemonObj${pokemonLength + 1}`,
     JSON.stringify(pokemonObj)
   );
-  window.location.href = "/Week2-0408/StoreAssignment/index.html";
+  window.location.href = "Chanwoo-jeong.github.io/Week2-0408/StoreAssignment/index.html";
 });
