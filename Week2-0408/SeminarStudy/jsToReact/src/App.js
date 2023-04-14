37ed8c85e8dcd01251274a58426260c3ee7e53d2
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-14 22:00:59 +0900

    [chore] : 의미없는 주석 삭제

--- Week2-0408/SeminarStudy/jsToReact/src/App.js
+++ Week2-0408/SeminarStudy/jsToReact/src/App.js
@@ -1,31 +1,3 @@
-// import MainPage from "./pages/main";
-// import Error404 from "./pages/error404";
-
-// const BASE_URL = "http://localhost:5174";
-// const ROUTE_LIST = [{ path: "/", element: MainPage }];
-
-
-// function App($container) {
-//     this.$container = $container;
-//     let currentPage = undefined;
-
-//     const init = () => {
-//         const findMatchedRoute = () => ROUTE_LIST.filter((route) => route.path === location.pathname)
-
-//         const route = () => {
-//             currentPage = null;
-//             console.log(findMatchedRoute())
-//             const TargetPage = findMatchedRoute()?.element;
-//             currentPage = new TargetPage(this.$container)
-//         }
-
-//         route()
-//     }
-//     init()
-// }
-
-// export default App
-
 import MainPage from "./pages/main";
 import Error404 from "./pages/error404";
 
