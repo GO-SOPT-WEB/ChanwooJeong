3f81167d8450e38b4779f634c8b3811c99049b94
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-23 15:37:21 +0900

    [style] : styled-components 설치 및 GlobalStyle 적용

--- Week3-0422/MatchCardsGame/src/Pages/MatchCardsGame.jsx
+++ Week3-0422/MatchCardsGame/src/Pages/MatchCardsGame.jsx
@@ -1,9 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  border: 5px solid black;
+  width: 100%;
+  height: 100vh;
+  
+  font-size: 1.6rem;
+`;
+
 const MatchCardsGame = () => {
-    return (
-        <>
-            MatchCardsGame
-        </>
-    );
+  return (
+    <>
+      <Wrapper>MatchCardsGame</Wrapper>
+    </>
+  );
 };
 
 export default MatchCardsGame;
