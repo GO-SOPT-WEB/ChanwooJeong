023490a312c74a7474cb26ffdb0e116d1c672f87
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-25 02:30:51 +0900

    [style] : 게임 클리어 했을 시 모달과 함께 축하 색종이 떨어지는 에니메이션 추가

--- Week3-0422/MatchCardsGame/src/components/Modal.jsx
+++ Week3-0422/MatchCardsGame/src/components/Modal.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 
 const ModalWrapper = styled.div`
@@ -24,13 +23,38 @@ const ModalInnerContent = styled.div`
   font-size: 7rem;
 
   button {
-      margin-top: 2rem;
-      padding:1rem;
-      border: none;
-      background-color: palevioletred;
-      color: white;
-      font-weight: 600;
-      cursor: pointer;
+    margin-top: 2rem;
+    padding: 1rem;
+    border: none;
+    background-color: palevioletred;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`;
+
+const Confetti = styled.div`
+  position: absolute;
+  top: -1rem;
+  left: ${() => Math.random() * 1000}rem;
+  width: 1rem;
+  height: 1rem;
+  background-color: rgb(
+    ${() => Math.floor(Math.random() * 256)},
+    ${() => Math.floor(Math.random() * 256)},
+    ${() => Math.floor(Math.random() * 256)}
+  );
+
+  animation: fall 3s linear;
+  animation-delay: ${() => Math.random() * 10}s;
+
+  @keyframes fall {
+    from {
+    }
+    to {
+      transform: translateY(100vh);
+      opacity: 0;
+    }
   }
 `;
 
@@ -40,8 +64,19 @@ const Modal = ({ children, setIsModalOpen, allReset }) => {
     setIsModalOpen(false);
   };
 
+  const manyConfetti = () => {
+    const Confetties = Array.from({ length: 1000 }).map((index) => {
+      return <Confetti key={index} />;
+    });
+    return Confetties;
+  };
+
+  const confettieArr = manyConfetti();
+  console.log(confettieArr);
+
   return (
     <ModalWrapper>
+      {confettieArr}
       <ModalInnerContent>
         {children}
         <button onClick={resetAndReturnToGame}>게임으로 돌아가기</button>
