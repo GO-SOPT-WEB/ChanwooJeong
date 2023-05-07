3c41ce9627072021a50c3ca6742897a659fcb298
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-07 21:23:56 +0900

    [style] : style 변경

--- Week4-0506/SearchWeather/src/pages/WeatherInfoSection.jsx
+++ Week4-0506/SearchWeather/src/pages/WeatherInfoSection.jsx
@@ -3,24 +3,33 @@ import { styled } from "styled-components";
 import WeatherCard from "../components/WeatherCard";
 
 const Wrapper = styled.div`
-    width: 100%;
-    padding: 2rem;
+  width: 100%;
+  padding: 2rem;
 
-    border: 2px solid violet;
+  border: 2px solid violet;
+`;
+const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  span {
     font-size: 3rem;
-`
-
+  }
+`;
 
 const WeatherInfoSection = () => {
+  const { period, area } = useParams();
 
-    const {period ,area} = useParams()
-
-    return (
-        <Wrapper>
-            {period} {area}
-            <Outlet />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <InfoBox>
+        <span>날씨예보 : {period === "day" ? "오늘" : "주간"} </span>
+        <span> 지역 : {area}</span>
+      </InfoBox>
+      <Outlet />
+    </Wrapper>
+  );
 };
 
 export default WeatherInfoSection;
