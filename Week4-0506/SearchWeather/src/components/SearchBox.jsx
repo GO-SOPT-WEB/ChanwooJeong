4ba9d3d13075487fe687af1e66ce0a726b60ce38
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-07 19:22:20 +0900

    [feat] : DropDown day or week

--- Week4-0506/SearchWeather/src/components/SearchBox.jsx
+++ Week4-0506/SearchWeather/src/components/SearchBox.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import { styled } from "styled-components";
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  height: 50rem;
   padding: 1rem;
   border: 3px solid purple;
 `;
+const PeriodDropDown = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  padding-left: 0;
 
+  select {
+    width: 10rem;
+    font-size: 2rem;
+  }
+`;
 const SearchInput = styled.input`
   width: 100%;
   height: 5rem;
@@ -22,6 +35,12 @@ const SearchInput = styled.input`
 const SearchBox = () => {
   return (
     <Wrapper>
+      <PeriodDropDown>
+        <select name="period" id="periodFilter">
+          <option value="day">Day</option>
+          <option value="week">Week</option>
+        </select>
+      </PeriodDropDown>
       <SearchInput placeholder={`지역을 영어로 검색해보세요!`} />
     </Wrapper>
   );
