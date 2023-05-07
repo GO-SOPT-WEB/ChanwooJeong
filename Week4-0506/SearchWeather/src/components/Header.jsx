5fca9d537c5ab5b132d1f1073c58ef2fff401451
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-08 02:25:15 +0900

    [style] : header 와 body 색 변경

--- Week4-0506/SearchWeather/src/components/Header.jsx
+++ Week4-0506/SearchWeather/src/components/Header.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { styled } from "styled-components";
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: 10rem;
-  padding: 2.5rem 2.5rem 5rem 2.5rem;
+  padding: 2rem;
 
   font-size: 5rem;
 
-  background: linear-gradient(to bottom, #87ceeb, #ffffff);
+  background-color: #1a73e8;;;
+  
 `;
 
 const Header = () => {
--- Week4-0506/SearchWeather/src/components/PageLayout.jsx
+++ Week4-0506/SearchWeather/src/components/PageLayout.jsx
@@ -4,6 +4,7 @@ import Header from "./Header";
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
+  background-color: #d9edff;
 `;
 
 const BodySection = styled.div`
