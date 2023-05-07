a384e501deace11745330c653513cb238a5c1f1e
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-07 21:23:13 +0900

    [feat] : api test코드

--- Week4-0506/SearchWeather/src/components/WeatherCardSection.jsx
+++ Week4-0506/SearchWeather/src/components/WeatherCardSection.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WeatherCard from "./WeatherCard";
 import { styled } from "styled-components";
+import { useParams } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,6 +16,23 @@ const Wrapper = styled.div`
 `;
 
 const WeatherCardSection = () => {
+  const { period, area } = useParams();
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric`
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
+  // 주간 날씨 조회
+  // https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric
+
+  // 오늘 날씨 조회
+  // https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric
+
   return (
     <Wrapper>
       <WeatherCard />
