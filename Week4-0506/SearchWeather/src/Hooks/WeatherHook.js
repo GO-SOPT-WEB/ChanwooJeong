afb9fa8c1a67dea2f5fafb7888be13fdf5435112
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-11 01:47:03 +0900

    [refactor] : axios then catch -> async await 로 변경

--- Week4-0506/SearchWeather/src/Hooks/WeatherHook.js
+++ Week4-0506/SearchWeather/src/Hooks/WeatherHook.js
@@ -7,27 +7,30 @@ const WeatherHook = (period, area) => {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true)
-    setIsError(false)
-    let url;
-    if (period === "week")
-      url = `https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${
-        import.meta.env.VITE_APP_WEATHER
-      }&units=metric`;
-    if (period === "day")
-      url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${
-        import.meta.env.VITE_APP_WEATHER
-      }&units=metric`;
+    const getWeatherData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      let url;
+      if (period === "week")
+        url = `https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${
+          import.meta.env.VITE_APP_WEATHER
+        }&units=metric`;
+      if (period === "day")
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${
+          import.meta.env.VITE_APP_WEATHER
+        }&units=metric`;
 
-    axios
-      .get(url)
-      .then((Response) => {
-        setData(Response.data);
-        setIsLoading(false);
-      })
-      .catch((Error) => {
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
         setIsError(true);
-      });
+      }
+
+      setIsLoading(false);
+    };
+    getWeatherData();
+    return ()=>{}
   }, [period, area]);
 
   return { data, isLoading, isError };
