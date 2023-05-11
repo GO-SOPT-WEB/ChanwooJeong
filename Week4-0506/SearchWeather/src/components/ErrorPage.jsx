df641fba10fc9194763ed0644f1243ba829f1f8d
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-11 23:13:32 +0900

    [style] : ErrorPage styling

--- Week4-0506/SearchWeather/src/components/ErrorPage.jsx
+++ Week4-0506/SearchWeather/src/components/ErrorPage.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { styled } from 'styled-components';
 
 const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     width: 100%;
-    height: 100%;
+    height: 100vh;
+
+    font-size: xx-large;
 `
 
 const ErrorPage = () => {
     return (
         <Wrapper>
-            Error 페이지 , 잘못된 접근입니다.
+            🌦️ Error 페이지 , 잘못된 접근입니다.
         </Wrapper>
     );
 };
