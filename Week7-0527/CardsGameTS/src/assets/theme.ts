66be06049ff7c874b9c914a717253498aebe2c67
Chanwoo-Jeong <lordroad38@naver.com>
2023-06-07 00:31:55 +0900

    [ type ] : theme type 선언

--- Week7-0527/CardsGameTS/src/assets/theme.ts
+++ Week7-0527/CardsGameTS/src/assets/theme.ts
@@ -1,14 +1,17 @@
-export const lightTheme = {
-    bgColor: "rgb(252, 233, 233)",
-    headerColor: "#f8f8f8",
-    textColor: "black",
-  };
-  
+interface ThemeProps {
+  bgColor: string;
+  headerColor: string;
+  textColor: string;
+}
 
-export const darkTheme = {
-    bgColor: "#7b9095",
-    headerColor: "#722f37",
-    textColor: "white",
-  };
-  
-  
+export const lightTheme: ThemeProps = {
+  bgColor: "rgb(252, 233, 233)",
+  headerColor: "#f8f8f8",
+  textColor: "black",
+};
+
+export const darkTheme: ThemeProps = {
+  bgColor: "#7b9095",
+  headerColor: "#722f37",
+  textColor: "white",
+};
